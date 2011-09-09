d7041759c91cad085a3a5e8dac041662618d5d0f
Lori M Olson <lori@wndx.com>
2011-09-08 18:05:49 -0600

    Update to Rails 3.0.5; Restructure form tag helper for future parameterization

--- powerux-plugin-app/config/routes.rb
+++ powerux-plugin-app/config/routes.rb
@@ -53,7 +53,7 @@ PoweruxPluginApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "birds#select"
 
   # See how all your routes lay out with "rake routes"
 
--- powerux-plugin-app/vendor/plugins/powerux_multiselect/lib/powerux-multiselect/form_helper.rb
+++ powerux-plugin-app/vendor/plugins/powerux_multiselect/lib/powerux-multiselect/form_helper.rb
@@ -34,22 +34,22 @@ module ActionView
         updated_options = rename_multiselect_option(options)
         select_match_options = add_match_options(updated_options)
         select_selected_options = add_selected_options(updated_options)
-        text_autocomplete_tag = source.nil? ? "" : text_field_tag( 'match', match || 'Enter match text', options.merge(:class => 'multiselecttext'))
-        content_tag(:div, updated_options.merge(:class => 'multiselect')) do
-          text_autocomplete_tag +
-          select_tag( name.to_s + "_match", values, select_match_options) +
-          select_tag( name.to_s + "_selected", nil, select_selected_options) +
-          content_tag( :div, :class => 'multiselectbuttons') do
-            submit_tag( 'Add', :name => 'match2selected') +
-            tag(:br) +
-            submit_tag( 'Add All', :name => 'match2selected', :class => 'all') +
-            tag(:br) +
-            submit_tag( 'Remove', :name => 'selected2match') +
-            tag(:br) +
-            submit_tag( 'Remove All', :name => 'selected2match', :class => 'all')
-          end +
-          hidden_field_tag( "#{name.to_s}_ids[]", nil, :class => 'multiselectids' )
-        end
+
+        button_tags = []
+        button_tags << submit_tag( 'Add', :name => 'match2selected')
+        button_tags << submit_tag( 'Add All', :name => 'match2selected', :class => 'all')
+        button_tags << submit_tag( 'Remove', :name => 'selected2match')
+        button_tags << submit_tag( 'Remove All', :name => 'selected2match', :class => 'all')
+        buttons = content_tag( :div, raw(button_tags.join(tag(:br))), :class => 'multiselectbuttons')
+
+        selects = []
+        selects << text_field_tag( 'match', match || 'Enter match text', options.merge(:class => 'multiselecttext')) unless source.nil?
+        selects << select_tag( name.to_s + "_match", values, select_match_options)
+        selects << select_tag( name.to_s + "_selected", nil, select_selected_options)
+        selects << buttons
+        selects << hidden_field_tag( "#{name.to_s}_ids[]", nil, :class => 'multiselectids' )
+
+        content_tag(:div, raw(selects.join), updated_options.merge(:class => 'multiselect'))
       end
     end
   end
